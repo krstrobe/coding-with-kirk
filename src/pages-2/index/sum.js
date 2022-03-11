41a05868f780fad9d94725d294b7e9102f15fa4e
Kris Strobeck <kris@strobeck.com>
2022-03-11 15:46:42 -0600

    added test 4 * 5 to sum.test.js

--- src/pages-2/index/sum.js
+++ src/pages-2/index/sum.js
@@ -1,8 +1,12 @@
 function ab (a, b) {
-  if(a === 4)
-  {
-return 'try again, clown.';
-  }
+//   if(a === 4)
+//   {
+// return 'try again, clown.';
+//   }
+if(a === 4)
+{
+return a * b;
+}
   return a + b;
 }
 module.exports = {ab};
